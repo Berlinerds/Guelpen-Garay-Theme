efe130a8de2f1fa79422fb60bf41f923c8ea202c
carlosapa <carlos_non86@msn.com>
2014-12-02 13:03:22 +0100

    Block menu interaction and events

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /*==== MENU DESPLEGABLE ====*/
 
 (function ($, d) {
-	console.log($);
-	var menu_block  = $('.menu_block');
-	var toggle_out  = $('.navigation_menu a');
-	var toggle_in   = $('.menu_close');
-	var menu_opened = false;
-	var classes = ['closed', 'opened'];
 	
-	var open = function (e) {
-		//$(e.target).preventDefault();
+	var menu_block  = $('.menu_block'),
+		toggle_out  = $('.navigation_menu a'),
+		toggle_in   = $('.menu_close'),
+		menu_opened = false,
+		classes = ['closed', 'opened'],
+		pseudo_window = (typeof (window.Snap) != 'undefined') ? $('#content_out') : $(window),
+	
+	open = function (e) {
 		if (menu_opened === false) {
 			if (menu_block.hasClass(classes[0])) {
 				menu_block.toggleClass(classes[0], false);
@@ -19,9 +19,9 @@
 		} else {
 			return false;
 		}
-	};
+	},
 
-	var close = function (e) {
+	close = function (e) {
 		if (menu_opened === true) {
 			if (menu_block.hasClass(classes[1])) {
 				menu_block.toggleClass(classes[1], false);
@@ -31,12 +31,39 @@
 		} else {
 			return false;
 		}
+	},
+
+	toggle = function (e) {
+		if (menu_opened === true) {
+			//case of opened menu which has to be closed.
+			if (menu_block.hasClass(classes[1])) {
+				menu_block.toggleClass(classes[1], false);
+				menu_block.toggleClass(classes[0], true);
+			}
+			menu_opened = false;
+
+		} else if (menu_opened === false) {
+			//case of closed menu which has to be opened.
+			if (menu_block.hasClass(classes[0])) {
+				menu_block.toggleClass(classes[0], false);
+				menu_block.toggleClass(classes[1], true);
+			}
+			menu_opened = true;
+		}
 	};
 
+	//events on buttons
 	toggle_in.on('click', close);
-	toggle_out.on('click', open);
+	toggle_out.on('click', toggle);
 
-	console.log(menu_block.classList);
-
-
+	//event on scroll
+	var scroll_offset = 0; 
+	pseudo_window.on('scroll', function (e) {
+		scroll_offset++;
+		if (menu_opened === true && scroll_offset >= 10) {
+			close(e);
+			scroll_offset = 0;
+		}
+	})
+	
 }.call(this, jQuery, document));
